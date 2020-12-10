5f70509612c2ec445c99c04f3aa5d6f92a443fb3
zhaohongliang1999 <3075814465@qq.com>
2020-12-10 10:43:24 +0800

    使用vant组件库

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 加载vant核心组件库
+import Vant from 'vant'
+// 加载Vant全局样式
+import 'vant/lib/index.css'
+// 加载全局样式
 import './styles/index.less'
+
+// 注册使用 Vant 组件库
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
