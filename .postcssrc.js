fc47aed0d355be36df790fd5555b54daee8d64ba
zhaohongliang1999 <3075814465@qq.com>
2020-12-10 14:24:23 +0800

    axios的配置

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,10 +1,13 @@
 module.exports = {
   plugins: {
-    'autoprefixer': {
-      browsers: ['Android >= 4.0', 'iOS >= 8']
-    },
+    // Vue-cli已经配置了
+    // 'autoprefixer': {
+    //   browsers: ['Android >= 4.0', 'iOS >= 8']
+    // },
     'postcss-pxtorem': {
-      rootValue: 37.5,
+      rootValue ({ file }) {
+        return file.indexOf('vant') !== -1 ? 37.5 : 75
+      },
       propList: ['*']
     }
   }
