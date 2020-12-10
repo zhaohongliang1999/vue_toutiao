a7bb38a3453f7c51cf2c05e9d582aea9cbbd8e13
zhaohongliang1999 <3075814465@qq.com>
2020-12-10 16:21:03 +0800

    实现基本登入

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,23 +3,17 @@
     <!-- 导航栏 -->
     <van-nav-bar class="page-nav-bar" title="登录" />
     <!-- /导航栏 -->
-
     <!-- 登录表单 -->
     <van-form @submit="onSubmit">
-      <van-field
-        name="用户名"
-        placeholder="请输入手机号"
-      >
+      <van-field name="用户名" placeholder="请输入手机号"  v-model="user.mobile">
         <i slot="left-icon" class="toutiao toutiao-shouji"></i>
       </van-field>
-      <van-field
-        type="password"
-        name="验证码"
-        placeholder="请输入验证码"
-      >
+      <van-field type="password" name="验证码" placeholder="请输入验证码" v-model="user.code">
         <i slot="left-icon" class="toutiao toutiao-yanzhengma"></i>
         <template #button>
-          <van-button class="send-sms-btn" round size="small" type="default">发送验证码</van-button>
+          <van-button class="send-sms-btn" round size="small" type="default"
+            >发送验证码</van-button
+          >
         </template>
       </van-field>
       <div class="login-btn-wrap">
@@ -32,24 +26,39 @@
   </div>
 </template>
 <script>
+import { login } from '@/api/user.js'
 export default {
-  name: 'LoginIndex',
+  name: "LoginIndex",
   components: {},
   props: {},
-  data () {
+  data() {
     return {
-    }
+      user: {
+        mobile: "13911111113",
+        code: "246810",
+      },
+    };
   },
   computed: {},
   watch: {},
-  created () {},
-  mounted () {},
+  created() {},
+  mounted() {},
   methods: {
-    onSubmit (values) {
-      console.log('submit', values)
+    async onSubmit () {
+      const user = this.user
+      try {
+        const res = await login(user)
+        console.log('登录成功',res);
+      } catch (err) {
+        if (err.response.status === 400) {
+          console.log('手机号或验证码错误');
+        } else {
+          console.log(err,'登录失败');
+        }
+      }
     }
-  }
-}
+  },
+};
 </script>
 <style scoped lang="less">
 .login-container {
