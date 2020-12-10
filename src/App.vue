bb58c286719e024a86979ac68edadb4359f2858e
zhaohongliang1999 <3075814465@qq.com>
2020-12-10 15:33:02 +0800

    登录界面的绘制

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
     <!-- 路由的出口 -->
-     <router-view />
-    <h1>黑马头条</h1>
-    <div>
-      <i class="toutiao toutiao-shipin"></i>
-    </div>
-  </div> 
+    <router-view />
+  </div>
 </template>
 (
 <script>
 export default {
-  name: 'App',
-}
+  name: "App",
+};
 </script>
 
 <style scoped lang="less"></style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="login-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar" title="登录" />
+    <!-- /导航栏 -->
+
+    <!-- 登录表单 -->
+    <van-form @submit="onSubmit">
+      <van-field
+        name="用户名"
+        placeholder="请输入手机号"
+      >
+        <i slot="left-icon" class="toutiao toutiao-shouji"></i>
+      </van-field>
+      <van-field
+        type="password"
+        name="验证码"
+        placeholder="请输入验证码"
+      >
+        <i slot="left-icon" class="toutiao toutiao-yanzhengma"></i>
+        <template #button>
+          <van-button class="send-sms-btn" round size="small" type="default">发送验证码</van-button>
+        </template>
+      </van-field>
+      <div class="login-btn-wrap">
+        <van-button class="login-btn" block type="info" native-type="submit">
+          登录
+        </van-button>
+      </div>
+    </van-form>
+    <!-- /登录表单 -->
+  </div>
+</template>
+<script>
+export default {
+  name: 'LoginIndex',
+  components: {},
+  props: {},
+  data () {
+    return {
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {
+    onSubmit (values) {
+      console.log('submit', values)
+    }
+  }
+}
+</script>
+<style scoped lang="less">
+.login-container {
+  .toutiao {
+    font-size: 37px;
+  }
+  .send-sms-btn {
+    width: 152px;
+    height: 46px;
+    line-height: 46px;
+    background-color: #ededed;
+    font-size: 22px;
+    color: #666;
+    padding: 0;
+  }
+  .login-btn-wrap {
+    padding: 53px 33px;
+    .login-btn {
+      background-color: #6db4fb;
+      border: none;
+    }
+  }
+}
+</style>
