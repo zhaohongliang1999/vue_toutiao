3390cb31ceb2dea091925a25434bc154cc6d5978
zhaohongliang1999 <3075814465@qq.com>
2020-12-12 11:12:30 +0800

    封装本地存储操作模块

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -57,7 +57,7 @@ export default {
   data() {
     return {
       user: {
-        mobile: '13911111111', // 13911111111
+        mobile: '13911111113', // 13911111111
         code: '246810' // 246810
       },
       userFormRules: {
