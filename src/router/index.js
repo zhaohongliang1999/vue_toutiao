1a6b9ccb636826828530e3f4fb428e0d8a1a7b57
zhaohongliang1999 <3075814465@qq.com>
2020-12-12 14:39:43 +0800

    个人中心TabBar处理

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,33 @@ const routes = [
     path: '/login',
     name: 'login',
     component: () => import('@/views/login')
+  },
+  {
+    path: '/',
+    // name: 'layout', // 拥有默认子路由时，此 name 没有意义
+    component: () => import('@/views/layout'),
+    children: [
+      {
+        path: '', // 默认子路由，只能有一个
+        name: 'home',
+        component: () => import('@/views/home')
+      },
+      {
+        path: '/qa',
+        name: 'qa',
+        component: () => import('@/views/qa')
+      },
+      {
+        path: '/video',
+        name: 'video',
+        component: () => import('@/views/video')
+      },
+      {
+        path: '/my',
+        name: 'my',
+        component: () => import('@/views/my')
+      }
+    ]
   }
 ]
 
