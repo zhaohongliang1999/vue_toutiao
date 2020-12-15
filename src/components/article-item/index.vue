10497aedac9efdef44de9c66af3c6bdc429fbeb6
zhaohongliang1999 <3075814465@qq.com>
2020-12-15 10:46:50 +0800

    样式调整

--- src/components/article-item/index.vue
+++ src/components/article-item/index.vue
@@ -1,29 +1,31 @@
 <template>
   <van-cell class="article-item">
-    <div slot="title" class="title">{{ article.title }}</div>
+    <div slot="title" class="title van-multi-ellipsis--l2">
+      {{ article.title }}
+    </div>
     <div slot="label">
+      <!-- 三张图片的情况 -->
       <div v-if="article.cover.type === 3" class="cover-wrap">
-        <div class="cover-item">
-          <van-image
-            v-for="(img, index) in article.cover.images"
-            :key="index"
-            width="100"
-            height="100"
-            :src="img"
-          />
+        <div
+          class="cover-item"
+          v-for="(img, index) in article.cover.images"
+          :key="index"
+        >
+          <van-image :src="img" fit="cover" class="cover-item-img" />
         </div>
       </div>
-      <div>
+      <!-- 文章信息 -->
+      <div class="label-info-wrap">
         <span>{{ article.aut_name }}</span>
-        <span>{{ article.comm_count }}</span>
+        <span>{{ article.comm_count }}评论</span>
         <span>{{ article.pubdate }}</span>
       </div>
     </div>
+    <!-- 右侧一张图片的情况 -->
     <van-image
+      class="right-cover"
       v-if="article.cover.type === 1"
       slot="default"
-      width="100"
-      height="100"
       :src="article.cover.images[0]"
     />
   </van-cell>
@@ -31,18 +33,61 @@
 
 <script>
 export default {
-  name: "ArticleItem",
-  components: {},
+  name: 'ArticleItem',
   props: {
     article: {
       type: Object,
-      required: true,
-    },
-  },
-  data() {
-    return {};
-  },
-};
+      required: true
+    }
+  }
+}
 </script>
 
-<style lang="less"></style>
+<style lang="less" scoped>
+.article-item {
+  .title {
+    font-size: 32px;
+    color: #3a3a3a;
+  }
+  .van-cell__title {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+  // 右侧的一张图片
+  .van-cell__value {
+    // https://developer.mozilla.org/zh-CN/docs/Web/CSS/flex
+    flex: unset;
+    // unset
+    // 属性是默认继承属性，该值等同于 inherit
+    // 该属性是非继承属性，该值等同于 initial
+    width: 232px;
+    height: 146px;
+    padding-left: 25px;
+  }
+  .right-cover {
+    width: 100%;
+    height: 146px;
+  }
+  .label-info-wrap span {
+    font-size: 22px;
+    color: #b4b4b4;
+    margin-right: 25px;
+  }
+  .cover-wrap {
+    display: flex;
+    padding: 30px 0;
+    .cover-item {
+      flex: 1;
+      height: 146px;
+      &:not(:last-child) {
+        padding-right: 4px;
+      }
+      .cover-item-img {
+        width: 100%;
+        height: 146px;
+      }
+    }
+  }
+}
+</style>
