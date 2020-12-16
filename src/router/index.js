b06c7719a656b2594de6b39362eea63341db9872
zhaohongliang1999 <3075814465@qq.com>
2020-12-16 14:44:38 +0800

    创建组件并配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ const routes = [
   {
     path: '/login',
     name: 'login',
-    component: () => import('@/views/login')
+    component: () => import('@/views/layout')
   },
   {
     path: '/',
@@ -35,6 +35,11 @@ const routes = [
         component: () => import('@/views/my')
       }
     ]
+  },
+  {
+    path : '/search',
+    name : 'search',
+    component : () => import('@/views/search')
   }
 ]
 
---
+++ src/views/search/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="search-container">搜索页面</div>
+</template>
+
+<script>
+export default {
+  name: 'SearchIndex'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
