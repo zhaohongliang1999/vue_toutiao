954978541e1ca3743712ee4df1215f93033b9529
zhaohongliang1999 <3075814465@qq.com>
2020-12-16 15:01:19 +0800

    处理页面展示逻辑

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -2,53 +2,56 @@
   <div class="search-container">
     <!-- 搜索栏 -->
     <form action="/">
+      <!-- 获取焦点时，显示联想建议或搜索历史（取决于有没有 searchText） -->
       <van-search
-        v-model="value"
+        v-model="searchText"
         show-action
         placeholder="请输入搜索关键词"
         background="#3296fa"
         @search="onSearch"
         @cancel="onCancel"
+        @focus="isResultShow = false"
       />
     </form>
-    <!-- 搜索历史 -->
-    <search-history/>
+    <search-result v-if="isResultShow" />
     <!-- 联想建议 -->
-    <search-suggestion/>
-    <!-- 搜索结果 -->
-    <search-result/>
+    <search-suggestion v-else-if="searchText" />
+    <!-- 搜索历史 -->
+    <search-history v-else />
   </div>
 </template>
 
 <script>
-import SearchHistory from './components/search-history'
-import SearchSuggestion from './components/search-suggestion'
-import SearchResult from './components/search-result'
+import SearchHistory from "./components/search-history";
+import SearchSuggestion from "./components/search-suggestion";
+import SearchResult from "./components/search-result";
 export default {
-name: 'SearchIndex',
- components: {
+  name: "SearchIndex",
+  components: {
     SearchHistory,
     SearchSuggestion,
-    SearchResult
+    SearchResult,
   },
-  data () {
+  data() {
     return {
-      value: ''
-    }
+      value: "",
+      searchText: "",
+      isResultShow: false, // 控制搜索结果的展示
+    };
   },
   methods: {
-    onSearch (val) {
-      console.log(val)
+    onSearch(val) {
+      // 展示搜索结果
+      this.isResultShow = true
     },
-    onCancel () {
-      this.$router.back()
-    }
-  }
-}
+    onCancel() {
+      this.$router.back();
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
-
 .search-container {
   .van-search__action {
     color: #fff;
