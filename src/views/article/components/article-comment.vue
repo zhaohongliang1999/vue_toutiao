386e17228fb8cd1704bee1213e6b301164583ac3
zhaohongliang1999 <3075814465@qq.com>
2020-12-20 10:31:10 +0800

    展示文章评论总数量

---
+++ src/views/article/components/article-comment.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    x
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -45,6 +45,11 @@
           ref="article-content"
         ></div>
         <van-divider>正文结束</van-divider>
+        <!-- 文章评论列表 -->
+        <comment-list
+          :source="article.art_id"
+          @onload-success="totalCommentCount = $event.total_count"
+        />
 
         <!-- 底部区域 -->
         <div class="article-bottom">
@@ -52,14 +57,14 @@
             >写评论</van-button
           >
           <!-- 这里在 info 替换成 badge -->
-          <van-icon name="comment-o" badge="123" color="#777" />
+          <van-icon name="comment-o" :badge="totalCommentCount" color="#777" />
           <!-- 文章收藏 -->
           <collect-article
             class="btn-item"
             v-model="article.is_collected"
             :article-id="article.art_id"
           />
-         <!-- 文章点赞 -->
+          <!-- 文章点赞 -->
           <like-article
             class="btn-item"
             v-model="article.attitude"
@@ -95,12 +100,14 @@ import { ImagePreview } from 'vant'
 import FollowUser from '@/components/follow-user'
 import CollectArticle from '@/components/collect-article'
 import LikeArticle from '@/components/like-article'
+import CommentList from './components/comment-list'
 export default {
   name: 'ArticleIndex',
   components: {
     FollowUser,
     CollectArticle,
-     LikeArticle
+    LikeArticle,
+    CommentList
   },
   props: {
     articleId: {
@@ -113,7 +120,8 @@ export default {
       article: {}, // 文章详情
       loading: true, // 加载中的状态
       errStatus: 0, // 失败的状态码
-      followLoading: false // 关注按钮的 loading 状态
+      followLoading: false, // 关注按钮的 loading 状态
+      totalCommentCount: 0
     }
   },
   computed: {},
